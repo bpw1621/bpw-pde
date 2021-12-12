28b7622fa25c7ef2c297df2acaa3f732b9b1955e
bpw1621 <bpw1621@gmail.com>
2021-12-12 05:58:04 -0500

    Moved accessor name constant to fix circular import

--- src/bpw_pde/__init__.py
+++ src/bpw_pde/__init__.py
@@ -3,10 +3,6 @@
 import logging
 from logging import NullHandler
 
-from .dataframe import DataframeAccessor
-from .index import IndexAccessor
-from .series import SeriesAccessor
-
 __author__ = 'Bryan Patrick Wood'
 __email__ = 'bpw1621@gmail.com'
 __version__ = '0.0a0'
@@ -14,3 +10,7 @@ __version__ = '0.0a0'
 logging.getLogger(__name__).addHandler(NullHandler())
 
 ACCESSOR_NAME = 'bpw'
+
+from .dataframe import DataframeAccessor
+from .index import IndexAccessor
+from .series import SeriesAccessor
